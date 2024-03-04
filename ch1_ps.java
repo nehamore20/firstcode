72f32cb739ea83761662266cd79aa0028e3dc46c
nehamore20 <147084020+nehamore20@users.noreply.github.com>
2024-03-04 22:54:51 +0530

    Add files via upload

---
+++ ch1_ps.java
@@ -0,0 +1,16 @@
+package com.company;
+public class ch1_ps{
+     public static void main(string[] args) {
+
+    float sub1= 45;
+    float sub2= 48;
+    float sub3= 56;
+    float cgpa = (sub1 + sub2 + sub3)/30;
+    system.out.println("cgpa");
+
+
+
+    }
+}
+
+
