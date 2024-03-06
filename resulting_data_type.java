8ba0da7f303ead7222c38daf711afca67fcb58dc
nehamore20 <147084020+nehamore20@users.noreply.github.com>
2024-03-06 22:32:13 +0530

    Add files via upload

---
+++ resulting_data_type.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class resulting_data_type {
+    public static void main(String[] args) {
+        byte x = 4;
+        int y = 5;
+        short v = 3;
+        int a = y+v;
+        float b = 6.4f + x;
+        System.out.println(b);
+    }
+}
+         
+     
+    
